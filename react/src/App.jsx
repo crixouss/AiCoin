967c768379322114f864ce10b789910c80444d22
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-07-26 12:40:08 +0200

    Creating navbar with logo

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import AiChat from "./components/ai/AiChat.jsx";
+import Navbar from "./components/navigation/Navbar.jsx";
 
 
 function App() {
     return (
        <>
+           <Navbar/>
            <AiChat/>
        </>
     );
