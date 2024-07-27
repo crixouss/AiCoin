2ac86f7bc6f107692ec6bdc7f0aecd0515ef12e9
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-07-27 22:25:08 +0200

    Adding "dark theme :O"

--- react/src/components/navigation/Navbar.jsx
+++ react/src/components/navigation/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return(
       <nav className={"sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80"}>
           <div className={"container px-4 mx-auto relative text-sm"}>
-              <div className={"flex justify-center items-center"}>
+              <div className={"flex justify-between items-center"}>
                   <div className={"flex items-center flex-shrink-0"}>
                       <img src={logo} alt="Coin Logo" className="w-1/4 h-1/4 mr-2" />
                       <span className={"text-xl tracking-tight"}>
@@ -27,11 +27,11 @@ const Navbar = () => {
                           </li>
                       ))}
                   </ul>
-                  <div className={"hidden lg:flex justify-center space-x-12 items-center"}>
+                  <div className={"hidden bg-main-light lg:flex justify-center space-x-12 items-center"}>
                       <a className={"py-2 px-3 border rounded-md"}>
                           Sign In
                       </a>
-                      <a className={"bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"}>
+                      <a className={"bg-gradient-to-r from-main-dark to-orange-800 py-2 px-3 rounded-md"}>
                           Create an account
                       </a>
                   </div>
