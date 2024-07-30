2acf003718faf556de3cfa98037d036adac4b1f8
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-07-30 21:07:03 +0200

    Some hero section and working on other thing

--- react/src/components/home/hero/Hero.jsx
+++ react/src/components/home/hero/Hero.jsx
@@ -1,9 +1,54 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
-  return(
-      <div className={"text-9xl"}>
-          GGS
-      </div>
-  )
+    return (
+        <div className="flex flex-col items-center mt-6 lg:mt-20">
+            <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+                Discover the Hidden Value in
+                <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+          {" "}
+                    Your Coins
+        </span>
+            </h1>
+            <p className="mt-10 text-lg text-center text-neutral-500 max-w-5xl leading-relaxed">
+                Have you ever wondered about the history or value of those old coins lying around your house? With
+                CoinGenius, you can turn your curiosity into knowledge. Simply take a picture of any coin, and our
+                advanced AI-powered app will instantly analyze it, providing you with detailed information and
+                up-to-date market value.
+            </p>
+            <div className="flex justify-center my-10">
+                <Link
+                    to="/"
+                    className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
+                >
+                    Start Analyzing
+                </Link>
+                <Link to="/" className="py-3 px-4 mx-3 rounded-md border">
+                    About us
+                </Link>
+            </div>
+            <div className="flex mt-10 justify-center">
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
+                >
+                    <source src={""} type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
+                >
+                    <source src={""} type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
