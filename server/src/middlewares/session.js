c874ed0c657eb5ddbb524d7485f91cf086687b34
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-08-03 17:46:17 +0200

    FULL TRYHARD MODE MAN

---
+++ server/src/middlewares/session.js
@@ -0,0 +1,25 @@
+const {verifyToken} = require("../services/serviseJwt");
+
+function session() {
+    return function (req,res, next) {
+        const token = req.cookies?.token;
+
+        if (token) {
+            try {
+                const sessionData = verifyToken(token)
+                req.user = {
+                    email: sessionData.email,
+                    _id: sessionData._id
+                };
+                res.locals.hasUser = true;
+            } catch (err) {
+                res.clearCookie('token')
+            }
+        }
+        next()
+    };
+}
+
+module.exports = {
+    session
+}
